7a6ea351d14da66a84953a444863206ce508aa1d
ghtak <ghtak@ghtakui-MacBook-Pro.local>
2017-10-29 12:10:46 +0900

    .

--- codex/include/codex/component/container.hpp
+++ codex/include/codex/component/container.hpp
@@ -14,8 +14,9 @@ public:
     void insert( const std::shared_ptr<ComponentT>& component ) {
         typedef typename detail::representation_component<ComponentT>::type component_type;
         _type_map[ component_type::component_id() ] = std::static_pointer_cast<void>(component);
-        if ( detail::is_interface<component_type>::value && 
-             !std::is_same< component_type , ComponentT >::value )
+        // 인터페이스 타입이면서 인터페이스 이외의 타입으로 추가시
+        if ( detail::is_interface<component_type>::value and 
+            not std::is_same< component_type , ComponentT >::value )
         {
             _type_map[ComponentT::component_id()] = std::static_pointer_cast<void>(component);
         }
@@ -27,8 +28,9 @@ public:
         typedef typename detail::component_trait<ComponentT>::info_type info_type;
         typedef typename detail::representation_component<ComponentT>::type representation_component_type;
         intptr_t cid = ComponentT::component_id();
-        if ( detail::is_interface<component_type>::value 
-            && !std::is_same< interface_type , info_type >::value )
+        // interface_type 의 하위 타입으로 요청시
+        if ( detail::is_interface<component_type>::value and 
+            std::is_same< normal_type , info_type >::value )
         {
             if ( _type_map.find(cid) != _type_map.end() ) {
                 return std::static_pointer_cast<ComponentT>(_type_map[ cid ]);
