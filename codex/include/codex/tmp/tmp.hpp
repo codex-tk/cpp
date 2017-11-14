d584bf5107a513b91692647b9187d4c3c59e9c32
ghtak <gwonhyeong.t@gmail.com>
2017-11-14 13:32:52 +0900

    reformat code

--- codex/include/codex/tmp/tmp.hpp
+++ codex/include/codex/tmp/tmp.hpp
@@ -11,35 +11,30 @@ struct empty_type{};
 
 template < typename ... Ts > struct type_list;
 template < std::size_t I , typename T > struct at;
-template < std::size_t I , typename Head , typename ... Ts > 
-struct at< I , type_list< Head , Ts ... > > 
-    : at< I - 1 , type_list< Ts ...> > {
-};
-template < typename Head , typename ... Ts  > 
-struct at< 0 , type_list< Head , Ts ... > >{
-    using type = Head;
-};
+template < std::size_t I , typename Head , typename ... Ts > struct at< I , type_list< Head , Ts ... > > 
+    : at< I - 1 , type_list< Ts ...> > { };
+template < typename Head , typename ... Ts  > struct at< 0 , type_list< Head , Ts ... > >{ using type = Head; };
 
-template < typename Head , typename ... Ts >
-struct pop_front { using type = type_list< Ts ... >; };
+template < typename Head , typename ... Ts > struct pop_front { using type = type_list< Ts ... >; };
 
-template < typename I , typename T >
-struct pop_back;
-
-template < std::size_t ... I , typename ... Ts >
-struct pop_back< std::index_sequence< I ... > , type_list< Ts ... > >{
-    using type = type_list< typename type_list< Ts ... >::template at<I>... >;
-};
+template < typename I , typename T > struct pop_back;
+template < std::size_t ... I , typename ... Ts > struct pop_back< 
+    std::index_sequence< I ... >  , type_list< Ts ... > >
+{ using type = type_list< typename type_list< Ts ... >::template at<I>... >; };
 
 template < typename ... Ts > struct type_list{
-    template < std::size_t I > using at = typename at< I , type_list< Ts ... > >::type;
+    template < std::size_t I > 
+    using at = typename at< I , type_list< Ts ... > >::type;
     using front = at<0>;
-    using back = at< sizeof ... (Ts ) - 1 >;
-    template < typename U > using push_front = type_list< U , Ts ... >;
-    template < typename U > using push_back = type_list< Ts ... , U >;
+    using back = at< sizeof ... (Ts) - 1 >;
+    template < typename U > 
+    using push_front = type_list< U , Ts ... >;
+    template < typename U > 
+    using push_back  = type_list< Ts ... , U >;
     using pop_front = typename pop_front< Ts ... >::type;
-    using pop_back = typename pop_back< std::make_index_sequence<sizeof...(Ts) - 1> , type_list< Ts... > >::type;
-    template < template < typename ... Ts0 > class  U > using rebind = U< Ts ... >;
+    using pop_back  = typename pop_back< std::make_index_sequence<sizeof...(Ts) - 1> , type_list< Ts... > >::type;
+    template < template < typename ... Ts0 > class  U > 
+    using rebind = U< Ts ... >;
 };
 
 
--- codex/include/codex/util/utility.hpp
+++ codex/include/codex/util/utility.hpp
@@ -8,36 +8,30 @@
 #include <cstdlib>
 #include <memory>
 #include <cxxabi.h>
-#else
-
+namespace codex {
+    static inline std::string demangle(const char* name) {
+        int status = -4; // some arbitrary value to eliminate the compiler warning
+        // enable c++11 by passing the flag -std=c++11 to g++
+        std::unique_ptr<char, void(*)(void*)> res {
+            abi::__cxa_demangle(name, NULL, NULL, &status),
+            std::free
+        };
+        return (status==0) ? res.get() : name ;
+    }
+}
+#else 
+namespace codex {
+    static inline std::string demangle(const char* name) {
+        return name;
+    }
+}
 #endif
 
 namespace codex {
-
-static std::string demangle(const char* name);
-
-template <class T>
-std::string pretty_type_name(const T& t) {
-    return demangle(typeid(t).name());
+    template <class T>
+    std::string pretty_type_name(const T& t) {
+        return demangle(typeid(t).name());
+    }
 }
-    
-#ifdef __GNUG__
 
-static std::string demangle(const char* name) {
-    int status = -4; // some arbitrary value to eliminate the compiler warning
-    // enable c++11 by passing the flag -std=c++11 to g++
-    std::unique_ptr<char, void(*)(void*)> res {
-        abi::__cxa_demangle(name, NULL, NULL, &status),
-        std::free
-    };
-    return (status==0) ? res.get() : name ;
-}
-#else
-// does nothing if not g++
-static std::string demangle(const char* name) {
-    return name;
-}
-#endif
-
-}
 #endif
